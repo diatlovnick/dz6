d074c99beb208ff4a61d0137e7ad9f39b84be63e
Nickolai <datlovnick@mail.ru>
2022-06-02 17:46:07 +0300

    сделаны две задачи

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+Console.WriteLine("Введите числа через запятую:");
+            string str = Console.ReadLine();
+
+            string[] array = str.Split(",");
+
+
+            int countOfPositive = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (int.TryParse(array[i], out int number) && number > 0)
+                    countOfPositive++;
+            }
+
+            Console.WriteLine($"количество положительных чисел, введеных с клавиатуры: {countOfPositive}");
+            
---
+++ task2/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+int GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double GetX(int k1, int k2, int b1, int b2)
+{
+    double b = b2 - b1;
+    double k = k1 - k2;
+    double x = b / k;
+    Console.WriteLine();
+    return x;
+}
+
+double GetY(int k1, int k2, int b1, int b2, double x)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+int k1 = GetNumber("введите k1");
+int k2 = GetNumber("введите k2");
+int b1 = GetNumber("введите b1");
+int b2 = GetNumber("введите b2");
+
+if (k1 != k2)
+{
+            
+    double x = GetX(k1, k2, b1, b2);
+    double y = GetY(k1, k2, b1, b2, x);
+    Console.WriteLine($"{x}; {y}");
+}
+
+else
+{
+    Console.WriteLine("Прямые параллельны");
+            
+}
+
+
